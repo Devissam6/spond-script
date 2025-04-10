e78df1d12356c778cf6ba2931c2910a5ac7cde07
Devissam6 <54173427+Devissam6@users.noreply.github.com>
2025-04-10 02:27:46 +0100

    test: add json output formatter

--- script.py
+++ script.py
@@ -3,16 +3,22 @@ from spond import spond
 from dotenv import load_dotenv
 import os
 
+from json_formatter import json_format
+
 load_dotenv()
 
 username = os.getenv('SPOND_USERNAME')
 password = os.getenv('SPOND_PASSWORD')
-group_id = os.getenv('GROUP_ID')
+group_id_jnp = os.getenv('GROUP_ID_JNP')
+group_id_archers = os.getenv('GROUP_ID_ARCHERS')
+group_id_test = os.getenv('GROUP_ID_TEST')
 
 async def main():
     s = spond.Spond(username=username, password=password)
-    group = await s.get_group(group_id)
-    print(group)
+    group = await s.get_group(group_id_test)
+    events = await s.get_events(group_id_test)
+    json_format(events, "Events_Test_Json")
+
     await s.clientsession.close()
 
 asyncio.run(main())
